0a7f548aecb73b138163778c7ca0da112aac3a0b
Arnab Biswas <arnab@toddleapp.com>
2024-05-02 09:38:47 +0000

    testing

--- index.js
+++ index.js
@@ -20,43 +20,43 @@ const _ = require("lodash");
 // const result = checkElements(arr1, arr2);
 // console.log(result);
 
-const childOrgs = [
-  {
-    fk_entity_id: "11013",
-    fk_tag_id: 23,
-  },
-  {
-    fk_entity_id: "12265",
-    fk_tag_id: 79,
-  },
-  {
-    fk_entity_id: "2374",
-    fk_tag_id: 3,
-  },
-  {
-    fk_entity_id: "2568",
-    fk_tag_id: 1,
-  },
-  {
-    fk_entity_id: "2616",
-    fk_tag_id: 23,
-  },
-  {
-    fk_entity_id: "4652",
-    fk_tag_id: 44,
-  },
-  {
-    fk_entity_id: "88570354422789353",
-    fk_tag_id: 23,
-  },
-  {
-    fk_entity_id: "9199",
-    fk_tag_id: 46,
-  },
-];
+// const childOrgs = [
+//   {
+//     fk_entity_id: "11013",
+//     fk_tag_id: 23,
+//   },
+//   {
+//     fk_entity_id: "12265",
+//     fk_tag_id: 79,
+//   },
+//   {
+//     fk_entity_id: "2374",
+//     fk_tag_id: 3,
+//   },
+//   {
+//     fk_entity_id: "2568",
+//     fk_tag_id: 1,
+//   },
+//   {
+//     fk_entity_id: "2616",
+//     fk_tag_id: 23,
+//   },
+//   {
+//     fk_entity_id: "4652",
+//     fk_tag_id: 44,
+//   },
+//   {
+//     fk_entity_id: "88570354422789353",
+//     fk_tag_id: 23,
+//   },
+//   {
+//     fk_entity_id: "9199",
+//     fk_tag_id: 46,
+//   },
+// ];
 
 // const regions = ["46", "23"];
-let childOrganizations = childOrgs;
+// let childOrganizations = childOrgs;
 
 // if (regions != null) {
 //   childOrganizations = childOrganizations
@@ -91,5 +91,34 @@ let childOrganizations = childOrgs;
 // );
 // console.log(co2);
 
-const cirl1 = _.map(childOrganizations, (org) => org.fk_entity_id);
-console.log(cirl1)
+// const cirl1 = _.map(childOrganizations, (org) => org.fk_entity_id);
+// console.log(cirl1)
+
+const defaultAMValues = {
+  parent_type: "INCIDENT",
+  fk_parent_id: "incidentId",
+  d: null,
+  organization_id: "orgId",
+  created_by: "jwtUserId",
+  created_at: "db.fn.now()",
+  updated_by: "jwtUserId",
+  updated_at: "db.fn.now()",
+};
+
+const insertData = [
+  {
+    ...defaultAMValues,
+    group_type: "STAFF",
+    fk_group_id: "1215",
+    detail_type: "CONFIDENTIAL_DETAIL",
+    value: "OWNER",
+  },
+  {
+    ...defaultAMValues,
+    group_type: "INVOLVED_STUDENT",
+    detail_type: "INCIDENT_DETAIL",
+    value: "NO_ACCESS",
+  }
+];
+
+console.log(insertData);
